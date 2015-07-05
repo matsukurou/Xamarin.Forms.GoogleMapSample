323a82d3c829ae8feb760036a643c36003830ca3
mattsuDev <dynamic.dramatic.developer@gmail.com>
2015-07-05 18:19:29 +0900

    Map表示用のページを追加

---
+++ MapSample/MapPage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+using Xamarin.Forms.Maps;
+
+namespace MapSample
+{
+	public class MapPage : ContentPage
+	{
+		public MapPage()
+		{
+			// マップの中心座標の設定
+			var centerPosition = new Position(35.658517, 139.701334);
+			// 中心座標からの表示距離（マップの縮尺）を設定
+			var distanceFromCenter = Distance.FromKilometers(0.5);
+
+			// マップスパンの生成
+			var mapSpan = MapSpan.FromCenterAndRadius(centerPosition, distanceFromCenter);
+
+			// 設定した内容でマップを生成
+			var map = new Map(mapSpan);
+
+			Content = map;
+		}
+	}
+}
+
+
